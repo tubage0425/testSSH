7246c1c1ded388662e32e378a84b53389bfca684
tubagehome <crepuscular@yeah.net>
2024-03-11 15:48:27 +0800

    commit first

---
+++ src/main/java/com/atguigu/Main.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author:
+ * @Create: 2024/3/11 14:30
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
